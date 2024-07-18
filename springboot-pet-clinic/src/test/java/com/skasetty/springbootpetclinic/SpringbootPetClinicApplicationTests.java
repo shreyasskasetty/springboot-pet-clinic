e3173fbdfcf1964ee3e72b59482f7979a4dcdfcd
shreyasskasetty-tamu <shreyas30kasetty@gmail.com>
2024-07-18 13:33:20 -0500

    Initial Project add

---
+++ springboot-pet-clinic/src/test/java/com/skasetty/springbootpetclinic/SpringbootPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skasetty.springbootpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
